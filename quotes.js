8a3a3bec7de2bbe901edcb7446a802e91b3bebc4
Gabrielle Kamph <gabrielle.kamph@gmail.com>
2021-03-19 21:10:17 +0100

    Creating array with quotes

---
+++ quotes.js
@@ -0,0 +1,17 @@
+// An array with all different quotes
+
+let quotesArray = [
+    {"author": "xx 1",
+    "quote": "This is the first quote"},
+
+    {"author": "xx 2",
+    "quote": "This is the secound quote"},
+
+    {"author": "xx 3",
+    "quote": "This is the third quote"},
+
+    {"author": "xx 4",
+    "quote": "This is the fourth quote"},
+];
+
+console.log(quotesArray);
