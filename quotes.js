7ebd2074a1da27e0606f58692f6edbcbb02189c0
Gabrielle Kamph <gabrielle.kamph@gmail.com>
2021-03-19 22:47:18 +0100

    Creating function and adding quotes

--- quotes.js
+++ quotes.js
@@ -1,17 +1,42 @@
 // An array with all different quotes
 
-let quotesArray = [
-    {"author": "xx 1",
-    "quote": "This is the first quote"},
+const quotesArray = [
+    {"author": "Oscar Wilde",
+    "quote": "Be yourself; everyone else is already taken."},
 
-    {"author": "xx 2",
-    "quote": "This is the secound quote"},
+    {"author": "Mark Twain",
+    "quote": "If you tell the truth, you don't have to remember anything."},
 
-    {"author": "xx 3",
-    "quote": "This is the third quote"},
+    {"author": "Allen Saunders",
+    "quote": "Life is what happens to us while we are making other plans."},
 
-    {"author": "xx 4",
-    "quote": "This is the fourth quote"},
+    {"author": "Thomas A. Edison",
+    "quote": "I have not failed. I've just found 10,000 ways that won't work."},
+
+    {"author": "George Eliot",
+    "quote": "It is never too late to be what you might have been."},
+
+    {"author": "Pablo Picasso",
+    "quote": "Everything you can imagine is real."},
+
+    {"author": "George Bernard Shaw",
+    "quote": "Life isn't about finding yourself. Life is about creating yourself."},
+
+    {"author": "Bob Marley",
+    "quote": "One good thing about music, when it hits you, you feel no pain."},
+
 ];
 
 console.log(quotesArray);
+
+// Function that generate quotes when button is clicked
+
+let btn = document.getElementById("btn");
+btn.addEventListener("click", generateQuote);
+
+function generateQuote() {
+    let random = Number.parseInt(Math.random()*quotesArray.length + 1);
+
+    document.getElementById("quote").innerHTML = "\"" + quotesArray[random].quote + "\"";
+    document.getElementById("author").innerHTML = "- " + quotesArray[random].author;
+}
